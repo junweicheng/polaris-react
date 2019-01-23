38019601db610fdaf069af7f4727edcd86639527
amrocha <andre.rocha@shopify.com>
2019-01-23 14:47:13 -0800

    more testing

--- build-consumer.sh
+++ build-consumer.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+echo "CLONING"
 git clone ssh://git@github.com/Shopify/$1
+echo "CDing"
 cd $1
+ls -l ..
+echo "YARNing"
 yarn
+echo "ADDing"
 yarn add file:../polaris-react
+echo "BUILDING"
 yarn run build
