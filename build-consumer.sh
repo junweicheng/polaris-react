f9aec19b78700dfceefe599532ae7bc3b8918f01
amrocha <andre.rocha@shopify.com>
2019-01-23 17:15:57 -0800

    Increase timeout, remove debug messages

--- build-consumer.sh
+++ build-consumer.sh
@@ -2,17 +2,11 @@
 
 ls -l
 ls -l ../
-pwd
-echo "CLONING"
 git clone ssh://git@github.com/Shopify/$1
-echo "CDing"
 cd $1
 ls -l
-echo "YARNing"
 yarn
-echo "ADDing"
 yarn add file:../polaris-react
-echo "BUILDING"
 yarn run build
 yarn run build:production
 
